0028703c8e4ce15dfe943b90107e74a41e2d8d80
Chris Hillery <ceej@couchbase.com>
2023-06-21 15:54:16 -0700

    CBD-5460: Logic to set PRODUCT_VERSION
    This will honor a command-line setting, or extract it from the manifest
    where possible.
    
    Change-Id: I9c06df8c34ea58cd43ff5aab723b0c7b566dc39f
    Reviewed-on: https://review.couchbase.org/c/couchbase-odbc-driver/+/192580
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Janhavi Tripurwar <janhavi.tripurwar@couchbase.com>

---
+++ cmake/get_version.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+Copyright 2017-Present Couchbase, Inc.
+
+Use of this software is governed by the Business Source License included in
+the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+file, in accordance with the Business Source License, use of this software will
+be governed by the Apache License, Version 2.0, included in the file
+licenses/APL2.txt.
+"""
+
+# See primary explanation for what this script does in CMakeLists.txt.
+
+# This script needs to work with python2 or python3. It also tries
+# to never throw an error depsite what manifest nonsense it might come across.
+# If it can't find the version for whatever reason, it outputs nothing.
+
+import os
+import sys
+import xml.etree.ElementTree as ET
+
+manifest = sys.stdin.read()
+if manifest == '':
+    # Failed to get manifest for some reason
+    sys.exit(0)
+
+root = ET.fromstring(manifest)
+
+verattr = root.find('project[@name="build"]/annotation[@name="VERSION"]')
+if verattr is not None:
+    version = verattr.get('value')
+    if version is not None:
+        print (version + "." + os.environ.get("BLD_NUM", "0"))
